ff94c804477f8ae2f42c05f47f0416f13f3c8c77
pmeter12 <77161353+pmeter12@users.noreply.github.com>
2022-04-19 23:05:09 +0200

    završen backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 //inicijaliziranje app
 const middlewares = require("./middlewares");
+const logs = require("./routes/log.route.js");
 const app=express();
 
 const DATABASE_CONNECTION = process.env.DATABASE_URL;
@@ -33,10 +34,12 @@ app.get('/', (req,res) => {
     });
 });
 
+app.use("/api/logs", logs);
+
 app.use(middlewares.nijePronaden);
 app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`Spojeni na http://localhost:${port}`);
+    console.log(`Spojeni smo na http://localhost:${port}`);
 });
